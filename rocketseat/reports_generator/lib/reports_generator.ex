8f903d6169bf963e74dae2477826001046ce96f9
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2022-10-15 21:26:34 -0300

    Add Rockseat's ReportsGenerator project

---
+++ rocketseat/reports_generator/lib/reports_generator.ex
@@ -0,0 +1,69 @@
+defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
+  @avaliable_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
+  @options ["users", "foods"]
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+  end
+
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Please provide a list od strings."}
+  end
+
+  def build_from_many(filenames) do
+    result =
+      filenames
+      |> Task.async_stream(&build/1)
+      |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+
+    {:ok, result}
+  end
+
+  def fetch_higher_cost(report, option) when option in @options do
+    result = Enum.max_by(report[option], fn {_key, value} -> value end)
+    {:ok, result}
+  end
+
+  def fetch_higher_cost(_report, _option), do: {:error, "Invalid option."}
+
+  defp sum_values([id, food_name, price], %{"users" => users, "foods" => foods} = report) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    %{report | "users" => users, "foods" => foods}
+  end
+
+  defp sum_reports(%{"users" => users1, "foods" => foods1}, %{
+         "users" => users2,
+         "foods" => foods2
+       }) do
+    users = merge_maps(users1, users2)
+    foods = merge_maps(foods1, foods2)
+
+    %{"users" => users, "foods" => foods}
+  end
+
+  defp merge_maps(map1, map2),
+    do: Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
+
+  defp report_acc do
+    users = Enum.into(1..30, %{}, fn id -> {Integer.to_string(id), 0} end)
+    foods = Enum.into(@avaliable_foods, %{}, &{&1, 0})
+
+    %{"users" => users, "foods" => foods}
+  end
+end
